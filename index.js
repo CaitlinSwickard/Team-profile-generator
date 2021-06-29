bd7d87f619e68732a7ccb8fe2c42e6ef4668bd45
Caitlin Swickard <caitlin6029@gmail.com>
2021-06-29 14:02:11 -0600

    build out of classes for each profile

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// init repo
+
+// packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateHTML = require('./src/generateHTML');
+
+
+// do we need to include all the questions from every profile??? like school, github, office number??
+const questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your first name?',
+    },
+    {
+      type: 'number',
+      name: 'id',
+      message: 'What is your id number?',
+    },
+    {
+      type: 'input',
+      name: 'password',
+      message: 'What is your email?',
+    },
+    // {
+    //   type: '',
+    //   name: '',
+    //   message: '',
+    // },
+  ];
+
+
+  // function to write generated HTML file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(fileName, data)
+};
+
+
+// is the generated file going to be a html file????? how do we link it??
+// function to initialize app
+function init() {
+  inquirer.prompt(questions)
+    .then(response => {
+      // console.log(response);
+      writeToFile('generated-index.html', generateHTML({ ...response }))
+    })
+};
+
+// Function call to initialize app
+init();
