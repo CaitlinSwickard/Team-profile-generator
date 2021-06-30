ed1308cbc40a5445eef4be30b43b35d4750232a5
Caitlin Swickard <caitlin6029@gmail.com>
2021-06-29 19:29:05 -0600

    build out of HTML and CSS

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
 // packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 const generateHTML = require('./src/generateHTML');
 
 
